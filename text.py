a08fc3b9cb11d5674a84c4bac7d57ee0bec8561e
tatra-code <62423145+tatra-code@users.noreply.github.com>
2020-03-20 11:34:04 +0100

    count_word_in_text function added to count words in the text.
    This function is used to validate the entered text against the word limit. If the number of words int the text is greater than the set limit, usrr is prompted to enter the text again.

--- text.py
+++ text.py
@@ -1,16 +1,23 @@
 ''' file to process text entered by user '''
-
+WORD_LIMIT = 50
 def get_text():
   text = input('Enter text: ')
   return text
 
 def show_text(text):
+  if count_word_in_text(text) >= WORD_LIMIT:
+    get_text()
   print(text)
+ 
+def count_word_in_text(text):
+  return text.count()
+  
 
 def main():
   user_entered_text = get_text()
   show_text()
-  
+ 
+
 if __name__ == "__main__":
   main()
   
